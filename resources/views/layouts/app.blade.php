0d1405f84d9cd6ea283980e93fbfcd009cdd8e17
sezaite <martyna.sezaite@gmail.com>
2021-03-28 17:10:07 +0100

    nav bar styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,30 +57,30 @@
                         @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                               Authors
-                                                           </a>
-                                                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                               <a class="dropdown-item" href="{{ route('author.index') }}">
-                                                                   Authors List
-                                                               </a>
-                                                               <a class="dropdown-item" href="{{ route('author.create') }}">
-                                                                   New Author
-                                                               </a>
-                                                           </div>
-                                                       </li>
-                                                       <li class="nav-item dropdown">
-                                                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                               Books
-                                                           </a>
-                                                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                               <a class="dropdown-item" href="{{ route('book.index') }}">
-                                                                   Books List
-                                                               </a>
-                                                                   <a class="dropdown-item" href="{{ route('book.create') }}">
-                                                                   New Book
-                                                               </a>
-                                                           </div>
-                                                       </li>
+                                    Authors
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('author.index') }}">
+                                        Authors List
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('author.create') }}">
+                                        New Author
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Books
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('book.index') }}">
+                                        Books List
+                                    </a>
+                                        <a class="dropdown-item" href="{{ route('book.create') }}">
+                                        New Book
+                                    </a>
+                                </div>
+                            </li>
                             
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
